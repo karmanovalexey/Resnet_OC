b74d41e7d6b555e47f9047fded8c62d41444d47a
alexey <karmanov.aa@phystech.edu>
2021-04-07 23:02:09 +0300

    docker

--- docker/start_and_into.sh
+++ docker/start_and_into.sh
@@ -1,7 +1,7 @@
 docker run \
        	--gpus all -it \
        	--rm \
-	--shm-size 8G \
+	    --ipc=host \
        	-v /home/yudin/alexey/Resnet_OC:/home/Resnet_OC \
 	-v /home/yudin/Datasets/Mapillary:/home/Mapillary \
 	alexey-pytorch
