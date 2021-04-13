7534b732bcfdfd65c874e0132710d8922c8894a2
alexey <karmanov.aa@phystech.edu>
2021-04-13 18:57:37 +0300

    merging

--- docker/start_and_into.sh
+++
@@ -1,7 +0,0 @@
-docker run \
-       	--gpus all -it \
-       	--rm \
-	    --ipc=host \
-       	-v /home/yudin/alexey/Resnet_OC:/home/Resnet_OC \
-	-v /home/yudin/Datasets/Mapillary:/home/Mapillary \
-	alexey-pytorch
