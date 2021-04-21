7bfd8b7be3839a8075f41c572925aac502dca867
alexey <karmanov.aa@phystech.edu>
2021-04-21 16:53:19 +0300

    added ocr

---
+++ resnet_ocr/ocr.py
@@ -0,0 +1,204 @@
+'''
+based on: https://github.com/openseg-group/OCNet.pytorch/tree/master/oc_module
+'''
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class SpatialGather_Module(nn.Module):
+    """
+        Aggregate the context features according to the initial 
+        predicted probability distribution.
+        Employ the soft-weighted method to aggregate the context.
+    """
+    def __init__(self, cls_num=0, scale=1):
+        super(SpatialGather_Module, self).__init__()
+        self.cls_num = cls_num
+        self.scale = scale
+
+    def forward(self, feats, probs):
+        batch_size, c, h, w = probs.size(0), probs.size(1), probs.size(2), probs.size(3)
+        probs = probs.view(batch_size, c, -1)
+        feats = feats.view(batch_size, feats.size(1), -1)
+        feats = feats.permute(0, 2, 1) # batch x hw x c 
+        probs = F.softmax(self.scale * probs, dim=2)# batch x k x hw
+        ocr_context = torch.matmul(probs, feats)\
+        .permute(0, 2, 1).unsqueeze(3)# batch x k x c
+        return ocr_context
+
+class SpatialOCR_Module(nn.Module):
+    """
+    Implementation of the OCR module:
+    We aggregate the global object representation to update the representation for each pixel.
+    """
+    def __init__(self, 
+                 in_channels, 
+                 key_channels, 
+                 out_channels, 
+                 scale=1, 
+                 dropout=0.1, 
+                 bn_type=None):
+        super(SpatialOCR_Module, self).__init__()
+        self.object_context_block = ObjectAttentionBlock2D(in_channels, 
+                                                           key_channels, 
+                                                           scale, 
+                                                           bn_type)
+        _in_channels = 2 * in_channels
+
+        self.conv_bn_dropout = nn.Sequential(
+            nn.Conv2d(_in_channels, out_channels, kernel_size=1, padding=0, bias=False),
+            ModuleHelper.BNReLU(out_channels, bn_type=bn_type),
+            nn.Dropout2d(dropout)
+        )
+
+    def forward(self, feats, proxy_feats):
+        context = self.object_context_block(feats, proxy_feats)
+
+        output = self.conv_bn_dropout(torch.cat([context, feats], 1))
+
+        return output
+
+class _ObjectAttentionBlock(nn.Module):
+    '''
+    The basic implementation for object context block
+    Input:
+        N X C X H X W
+    Parameters:
+        in_channels       : the dimension of the input feature map
+        key_channels      : the dimension after the key/query transform
+        scale             : choose the scale to downsample the input feature maps (save memory cost)
+        bn_type           : specify the bn type
+    Return:
+        N X C X H X W
+    '''
+    def __init__(self, 
+                 in_channels, 
+                 key_channels, 
+                 scale=1, 
+                 bn_type=None):
+        super(_ObjectAttentionBlock, self).__init__()
+        self.scale = scale
+        self.in_channels = in_channels
+        self.key_channels = key_channels
+        self.pool = nn.MaxPool2d(kernel_size=(scale, scale))
+        self.f_pixel = nn.Sequential(
+            nn.Conv2d(in_channels=self.in_channels, out_channels=self.key_channels,
+                kernel_size=1, stride=1, padding=0, bias=False),
+            ModuleHelper.BNReLU(self.key_channels, bn_type=bn_type),
+            nn.Conv2d(in_channels=self.key_channels, out_channels=self.key_channels,
+                kernel_size=1, stride=1, padding=0, bias=False),
+            ModuleHelper.BNReLU(self.key_channels, bn_type=bn_type),
+        )
+        self.f_object = nn.Sequential(
+            nn.Conv2d(in_channels=self.in_channels, out_channels=self.key_channels,
+                kernel_size=1, stride=1, padding=0, bias=False),
+            ModuleHelper.BNReLU(self.key_channels, bn_type=bn_type),
+            nn.Conv2d(in_channels=self.key_channels, out_channels=self.key_channels,
+                kernel_size=1, stride=1, padding=0, bias=False),
+            ModuleHelper.BNReLU(self.key_channels, bn_type=bn_type),
+        )
+        self.f_down = nn.Sequential(
+            nn.Conv2d(in_channels=self.in_channels, out_channels=self.key_channels,
+                kernel_size=1, stride=1, padding=0, bias=False),
+            ModuleHelper.BNReLU(self.key_channels, bn_type=bn_type),
+        )
+        self.f_up = nn.Sequential(
+            nn.Conv2d(in_channels=self.key_channels, out_channels=self.in_channels,
+                kernel_size=1, stride=1, padding=0, bias=False),
+            ModuleHelper.BNReLU(self.in_channels, bn_type=bn_type),
+        )
+
+    def forward(self, x, proxy):
+        batch_size, h, w = x.size(0), x.size(2), x.size(3)
+        if self.scale > 1:
+            x = self.pool(x)
+
+        query = self.f_pixel(x).view(batch_size, self.key_channels, -1)
+        query = query.permute(0, 2, 1)
+        key = self.f_object(proxy).view(batch_size, self.key_channels, -1)
+        value = self.f_down(proxy).view(batch_size, self.key_channels, -1)
+        value = value.permute(0, 2, 1)
+
+        sim_map = torch.matmul(query, key)
+        sim_map = (self.key_channels**-.5) * sim_map
+        sim_map = F.softmax(sim_map, dim=-1)   
+
+        # add bg context ...
+        context = torch.matmul(sim_map, value)
+        context = context.permute(0, 2, 1).contiguous()
+        context = context.view(batch_size, self.key_channels, *x.size()[2:])
+        context = self.f_up(context)
+        if self.scale > 1:
+            context = F.interpolate(input=context, size=(h, w), mode='bilinear', align_corners=ALIGN_CORNERS)
+
+        return context
+
+
+class ObjectAttentionBlock2D(_ObjectAttentionBlock):
+    def __init__(self, 
+                 in_channels, 
+                 key_channels, 
+                 scale=1, 
+                 bn_type=None):
+        super(ObjectAttentionBlock2D, self).__init__(in_channels,
+                                                     key_channels,
+                                                     scale, 
+                                                     bn_type=bn_type)
+
+
+class OCR_Module(nn.Module):
+    """
+    Implementation of the BaseOC module
+    Parameters:
+        in_features / out_features: the channels of the input / output feature maps.
+        dropout: we choose 0.05 as the default value.
+        size: you can apply multiple sizes. Here we only use one size.
+    Return:
+        features fused with Object context information.
+    """
+    def __init__(self, in_channels, out_channels, mid_channels, key_channels, num_classes, dropout, sizes=([1])):
+        super(OCR_Module, self).__init__()
+        self.conv3x3_ocr = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels,
+                      kernel_size=3, stride=1, padding=1),
+            BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=relu_inplace),
+        )
+        self.ocr_gather_head = SpatialGather_Module(num_classes)
+
+        self.ocr_distri_head = SpatialOCR_Module(in_channels=mid_channels,
+                                                 key_channels=key_channels,
+                                                 out_channels=mid_channels,
+                                                 scale=1,
+                                                 dropout=0.05,
+                                                 )
+        self.cls_head = nn.Conv2d(
+            mid_channels, num_classes, kernel_size=1, stride=1, padding=0, bias=True)
+
+        self.aux_head = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels,
+                      kernel_size=1, stride=1, padding=0),
+            BatchNorm2d(in_channels),
+            nn.ReLU(inplace=relu_inplace),
+            nn.Conv2d(in_channels, num_classes,
+                      kernel_size=1, stride=1, padding=0, bias=True)
+        )
+        
+    def forward(self, feats):
+        out_aux_seg = []
+
+        # ocr
+        out_aux = self.aux_head(feats)
+        # compute contrast feature
+        feats = self.conv3x3_ocr(feats)
+
+        context = self.ocr_gather_head(feats, out_aux)
+        feats = self.ocr_distri_head(feats, context)
+
+        out = self.cls_head(feats)
+
+        out_aux_seg.append(out_aux)
+        out_aux_seg.append(out)
+
+        return out_aux_seg
+    
---
+++ resnet_ocr/resnet_ocr.py
@@ -0,0 +1,51 @@
+import torchvision
+
+from torch import nn
+import torch.nn.functional as F
+from collections import OrderedDict
+
+from .ocr import OCR_Module
+
+from .resnet_backbone import Resnet34
+
+NUM_CLASSES = 66
+
+def get_resnet34_ocr(num_classes=NUM_CLASSES):
+
+    replace_stride_with_dilation = [False, False, False]
+    inplanes_scale_factor = 4
+    
+    
+    inplanes = 1024 // inplanes_scale_factor
+    outplanes = 512
+    
+    backbone = Resnet34()
+    model = ResNet_Base_OC(backbone, inplanes, outplanes, num_classes)
+    
+    return model
+
+
+class ResNet_Base_OC(nn.Module):
+    def __init__(self, backbone, inplanes, outplanes, num_classes):
+        super(ResNet_Base_OC, self).__init__()
+        self.backbone = backbone
+        self.ocr = OCR_Module(in_channels=inplanes, 
+                          out_channels=outplanes, 
+                          key_channels=outplanes // 2, 
+                          mid_channels=outplanes // 2,
+                          num_classes=NUM_CLASSES, 
+                          dropout=0.05, 
+                          sizes=([1]))
+        
+    def forward(self, x):
+        input_shape = x.shape[-2:]
+        
+        #print('input: ', x.shape)
+        x = self.backbone(x)
+        # print('backbone: ', x.shape)
+        aux, x = self.ocr(x)
+        # print('context: ', x.shape)
+        x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=True)
+        # print('output: ', x.shape)
+        
+        return aux, x
