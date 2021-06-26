4b92f1b2851340dc426136e0c3714a9a1a6e7939
alexey <karmanov.aa@phystech.edu>
2021-06-26 10:39:46 +0300

    das

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,6 @@ docker run \
             --gpus all -it \
             --rm \
             --ipc=host \
-            -v /media/alexey/HDD5/Mapillary:/workspace/Mapillary \
-            -v /home/alexey/development/Resnet_OC:/workspace/Resnet_OC \
-            -v /home/karmanov_aa/taganrog:/workspace/video \
+            -v /home/raaicv/karmanov/Mapillary:/workspace/Mapillary \
+            -v /home/raaicv/karmanov/Resnet_OC:/workspace/Resnet_OC \
             resnet_oc
